6b7650ec40b91df5e38a3f456e54fedcdd5282d4
qatta93 <panasiuk.patrycja@gmail.com>
2022-06-02 17:48:21 +0200

    adding tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,42 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useEffect, useState, useRef } from 'react'
+   // @ts-ignore: Unreachable code error
+import WAVES from "vanta/dist/vanta.waves.min.js";
+import * as THREE from "three";
+import { ItemCard } from '../src/components/ItemCard'
 
 const Home: NextPage = () => {
+  const [vantaEffect, setVantaEffect] = useState<any>(0);
+  const vantaRef = useRef(null);
+  useEffect(() => {
+    if(!vantaEffect){
+      setVantaEffect(
+        WAVES({
+          el: vantaRef.current,
+          THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.00,
+          minWidth: 200.00,
+          scale: 1.00,
+          scaleMobile: 1.00,
+          // color: 0x2e9d89,
+          color: 0x6ca297,
+          shininess: 57.00,
+          waveHeight: 21.50,
+          waveSpeed:  0.55,
+          zoom: 1.09,
+        })
+      )
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destory();
+    }
+  },[vantaEffect])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,58 +46,25 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main className={styles.main} >
+        <section className={styles.main__intro} ref={vantaRef}>
+          <nav className={styles.main__nav} >
+            <p>LOGIN</p>
+            <p>CHART</p>
+          </nav>
+          <header className={styles.main__header}>
+            <h1>3D FURNITURE</h1>
+            <h2>Find your perfect match!</h2>
+          </header>
+        </section>
+        <div className={styles.main__items}>
+          <h1>Products:</h1>
+          <ItemCard />
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+
       </footer>
     </div>
   )
