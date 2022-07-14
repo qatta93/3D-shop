756b59f22a1d490c6ac7b937e636420964bb18ae
qatta93 <panasiuk.patrycja@gmail.com>
2022-07-14 12:18:20 +0200

    remove vanta

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useEffect, useState, useRef } from 'react'
-   // @ts-ignore: Unreachable code error
-import WAVES from "vanta/dist/vanta.waves.min";
+
 import * as THREE from "three";
 import { ItemCard } from '../src/components/ItemCard'
 import { FurnitureProps } from '@/components/types';
 
 
 const Home: NextPage = () => {
-  const [vantaEffect, setVantaEffect] = useState<any>(0);
-  const vantaRef = useRef(null);
-  useEffect(() => {
-    if(!vantaEffect){
-      setVantaEffect(
-        WAVES({
-          el: vantaRef.current,
-          THREE,
-          mouseControls: true,
-          touchControls: true,
-          gyroControls: false,
-          minHeight: 200.00,
-          minWidth: 200.00,
-          scale: 1.00,
-          scaleMobile: 1.00,
-          // color: 0x2e9d89,
-          color: 0x88267,
-          shininess: 37.00,
-          waveHeight: 21.50,
-          waveSpeed:  0.55,
-          zoom: 1.09,
-        })
-      )
-    }
-    return () => {
-      if (vantaEffect) vantaEffect.destory();
-    }
-  },[vantaEffect])
+
 
 
   // add some database 
@@ -63,7 +35,7 @@ const Home: NextPage = () => {
   return (
     <div className='w-screen h-screen'>
       <main className='h-screen relative flex-1'>
-        <section className='w-screen h-4/5 flex flex-column py-12 bg-slate-500' ref={vantaRef}>
+        <section className='w-screen h-4/5 flex flex-column py-12 bg-slate-500'>
           <header className='w-screen py-4 px-6 my-auto text-center text-white'>
             <h1 className='mb-4 text-4xl font-bold'>3D FURNITURE</h1>
             <h2 className='italic text-2xl'>Find your perfect match!</h2>
