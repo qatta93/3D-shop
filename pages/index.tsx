f35ce3f0ef879c95714c8440d12250e9e136455c
qatta93 <panasiuk.patrycja@gmail.com>
2022-07-15 11:06:51 +0200

    gif animation added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useEffect, useState, useRef } from 'react'
-
-import * as THREE from "three";
 import { ItemCard } from '../src/components/ItemCard'
 import { FurnitureProps } from '@/components/types';
 
 
 const Home: NextPage = () => {
+  const [showGif, setShowGif] = useState<boolean>(true);
 
+  useEffect(() => {
+    setTimeout(function() {
+      setShowGif(false)
+    }, 5500)
+  }, [])
 
 
   // add some database 
@@ -33,24 +37,26 @@ const Home: NextPage = () => {
 ]
 
   return (
-    <div className='w-screen h-screen'>
-      <main className='h-screen relative flex-1'>
-        <section className='w-screen h-4/5 flex flex-column py-12 bg-slate-500'>
-          <header className='w-screen py-4 px-6 my-auto text-center text-white'>
-            <h1 className='mb-4 text-4xl font-bold'>3D FURNITURE</h1>
-            <h2 className='italic text-2xl'>Find your perfect match!</h2>
-          </header>
-        </section>
-        <div>
-          <h1 className='text-3xl text-center my-8 text-zinc-600'>Products:</h1>
-          {furniture.map(item => <ItemCard key={item.id} item={item}/>)}
-        </div>
-      </main>
+      <div className='w-screen h-screen'>
+        <main className='h-screen relative flex-1'>
+          <section className='w-screen h-4/5 flex flex-column pb-12'>
+            <header className='h-auto w-screen text-center text-black relative my-2'>
+              <img src="images/interior2.gif" alt="interior" className='w-screen absolute inset-0 top-2'/>
+               {showGif && <img src="images/interior.gif" alt="interior" className='w-screen absolute top-28'/>}   
+              <h1 className='mb-4 text-4xl font-bold'>3D FURNITURE</h1>
+              <h2 className='italic text-2xl'>Find your perfect match!</h2>
+            </header>
+          </section>
+          <div>
+            <h1 className='text-3xl text-center my-4 text-zinc-600'>Products:</h1>
+            {furniture.map(item => <ItemCard key={item.id} item={item}/>)}
+          </div>
+        </main>
 
-      <footer>
+        <footer>
 
-      </footer>
-    </div>
+        </footer>
+      </div>
   )
 }
 
