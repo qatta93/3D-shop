7a84f2e578bd58385082def0d2d50b83bed8c832
qatta93 <panasiuk.patrycja@gmail.com>
2022-07-27 19:13:27 +0200

    test animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,12 +19,12 @@ const Home: NextPage = () => {
       tl.to(textRef.current, {opacity: 1, y: 0});
 
     const tl2 = gsap.timeline({delay: 6.3});
-      tl2.from(bcgRef.current, {opacity: 0, x: -640});
+      tl2.from(bcgRef.current, {opacity: 0, x: -640, y:0});
       tl2.to(bcgRef.current, {opacity: .9, x: 0, duration: .4});
 
-    const tl3 = gsap.timeline({delay: 6, duration: 3});
+    const tl3 = gsap.timeline({delay: 7});
       tl3.from(bcgDarkRef.current, {opacity: 0});
-      tl3.to(bcgDarkRef.current, {opacity: .3});
+      tl3.to(bcgDarkRef.current, {opacity: .3, duration: 1});
 
     setTimeout(function() {
       setShowGif(false)
@@ -34,12 +34,12 @@ const Home: NextPage = () => {
 
   return (
       <div className='w-screen h-full relative'>
-        <main className='h-full relative flex-1'>
+        <main className='h-full flex-1'>
           <section className='w-screen h-[480px] sm:h-[650px] md:h-[870px] lg:h-[500px] xl:h-[620px] flex drop-shadow-lg'>
             <div className="relative w-screen overflow-hidden text-slate-800 text-center">
               <img src="images/interior_bcg.png" alt="Avatar" className="absolute object-cover w-full h-full opacity-10" />
               <img src="images/hexagon2.jpg" alt="Avatar" className="absolute object-cover w-full h-full opacity-40" />
-              <img src="images/interior_bcg.png" alt="Avatar" className="absolute object-cover w-full h-full z-1" ref={bcgDarkRef}/>
+              <img src="images/interior_bcg.png" alt="Avatar" className="absolute object-cover w-full h-full z-1 opacity-0" ref={bcgDarkRef}/>
               {showGif && <img src="images/interior.gif" alt="interior" className='w-screen lg:w-3/5 lg:h-full absolute shadow-2xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10'/>}
               <img src="images/interior_static.png" alt="interior" className='w-screen lg:w-3/5 lg:h-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 '/>
               <img src="images/interior_bcg_slide.png" alt="interior" className='w-screen lg:w-3/5 lg:h-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0' ref={bcgRef}/>
