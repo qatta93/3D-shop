13072eb664ac750d2ec2962eb4e25184324494c8
qatta93 <panasiuk.patrycja@gmail.com>
2022-07-27 18:46:50 +0200

    test animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,21 +14,21 @@ const Home: NextPage = () => {
 
   useEffect(() => {
 
-    const tl = gsap.timeline({delay: 7.2, duration: 1});
-      tl.from(textRef.current, {opacity: 0, y: 100});
-      tl.to(textRef.current, {opacity: 1, duration: 1, y: 0});
+    const tl = gsap.timeline({delay: 7.6, duration: .4});
+      tl.from(textRef.current, {opacity: 0, y: 150, duration: .3});
+      tl.to(textRef.current, {opacity: 1, y: 0});
 
-    const tl2 = gsap.timeline({delay: 10.3, duration: 1});
-      tl2.from(bcgRef.current, {opacity: 0, x: -640, duration: 1});
-      tl2.to(bcgRef.current, {opacity: .9, x: 0});
+    const tl2 = gsap.timeline({delay: 6.3});
+      tl2.from(bcgRef.current, {opacity: 0, x: -640});
+      tl2.to(bcgRef.current, {opacity: .8, x: 0, duration: .4});
 
-    const tl3 = gsap.timeline({delay: 6.2, duration: 3});
-      tl3.from(bcgDarkRef.current, {opacity: 0, duration: 3});
+    const tl3 = gsap.timeline({delay: 6, duration: 3});
+      tl3.from(bcgDarkRef.current, {opacity: 0});
       tl3.to(bcgDarkRef.current, {opacity: .3});
 
     setTimeout(function() {
       setShowGif(false)
-    }, 6500)
+    }, 6000)
   }, [])
 
 
@@ -39,7 +39,7 @@ const Home: NextPage = () => {
             <div className="relative w-screen overflow-hidden text-slate-800 text-center">
               <img src="images/interior_bcg.png" alt="Avatar" className="absolute object-cover w-full h-full opacity-10" />
               <img src="images/hexagon2.jpg" alt="Avatar" className="absolute object-cover w-full h-full opacity-40" />
-              {/* <img src="images/interior_bcg.png" alt="Avatar" className="absolute object-cover w-full h-full z-1" ref={bcgDarkRef}/> */}
+              <img src="images/interior_bcg.png" alt="Avatar" className="absolute object-cover w-full h-full z-1" ref={bcgDarkRef}/>
               {showGif && <img src="images/interior.gif" alt="interior" className='w-screen lg:w-3/5 lg:h-full absolute shadow-2xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-80'/>}
               {!showGif && <img src="images/interior_static.png" alt="interior" className='w-screen lg:w-3/5 lg:h-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 '/>}
               <img src="images/interior_bcg_slide.png" alt="interior" className='w-screen lg:w-3/5 lg:h-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0' ref={bcgRef}/>
