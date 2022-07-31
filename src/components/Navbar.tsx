a262a8d1bddfc1e444c0feee2bd7915757924a11
qatta93 <panasiuk.patrycja@gmail.com>
2022-07-31 22:53:11 +0200

    logo bug fied

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { UserIcon } from '@heroicons/react/solid'
 import { ShoppingCartIcon } from '@heroicons/react/solid'
 import Link from 'next/link'
 import { signIn, signOut, useSession } from 'next-auth/react';
+import Image from 'next/image'
 
 export const Navbar = () => {
   const { data: session } = useSession();
@@ -10,7 +11,9 @@ export const Navbar = () => {
   return (
     <nav className='px-6 pt-4 text-zinc-600 border-b-[1px] border-zinc-600 bg-white'>
       <section className='flex flex-row justify-between pb-4'>
-        <Link href="/"><img src="images/logo.png" alt="logo" className='h-8 w-auto cursor-pointer'/></Link>
+        <Link href="/">
+          <Image src="/images/logo.png" alt="logo"  height={40} width={160} className='cursor-pointer'/>
+        </Link>
         <div className='flex flex-row'>
           <div>
             {!session ? (
