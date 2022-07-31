e63fa3c62b60344bf391085445c66fcee49f6801
qatta93 <panasiuk.patrycja@gmail.com>
2022-07-31 22:34:31 +0200

    nav responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,12 +8,14 @@ export const Navbar = () => {
 
   return (
     <nav className='flex flex-row px-6 py-4 text-zinc-600 justify-between border-b-[1px] border-zinc-600 bg-white'>
-      <Link href="/"><img src="images/logo.png" alt="logo" className='h-8 w-auto cursor-pointer'/></Link>
+      <Link href="/">
+        <img src="images/logo.png" alt="logo" className='h-8 w-auto cursor-pointer'/>
+      </Link>
       <div className='flex flex-row'>
         <UserIcon className="h-8 w-8 mx-2"/>
-        <p className='text-xl mr-4 hidden md:block'>LOGIN</p>
+        <p className='text-xl mr-4 hidden sm:block'>LOGIN</p>
         <ShoppingCartIcon className="h-8 w-8 sm:mx-2"/>
-        <p className='text-xl hidden md:block'>SHOP</p>
+        <p className='text-xl hidden sm:block'>SHOP</p>
       </div>
     </nav>
   )
