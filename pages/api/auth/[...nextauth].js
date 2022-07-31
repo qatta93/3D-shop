affe2ef8036df02d04e5bee862fa2b41fd955567
qatta93 <panasiuk.patrycja@gmail.com>
2022-07-31 20:53:02 +0200

    prisma migration generated

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,9 @@ import NextAuth from "next-auth";
 // import Providers from "next-auth/providers";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import Providers from 'next-auth/providers';
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import prisma from 'lib/prisma'
 
 export default NextAuth({
   secret: process.env.SECRET,
@@ -21,7 +24,28 @@ export default NextAuth({
         }
       }
     }),
+    Providers.Email({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
   ],
+
+  database: process.env.DATABASE_URL,
+  secret: process.env.SECRET,
+
+  session: {
+    jwt: true,
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+
+  jwt: {
+    secret: 'INp8IvdIyeMcoGAgFGoA61DdBglwwSqnXJZkgz8PSnX', //use a random secret token here
+    encryption: true,
+  },
+
+  debug: true,
+  adapter: PrismaAdapter(prisma),
+  
   pages: {
     signIn: '/auth/signin',
     signOut: '/auth/signout',
